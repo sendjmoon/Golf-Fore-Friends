d13d165a2181abceb5f32d5ae25054b4ed43c29f
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-06-20 17:56:13 -0700

    setup initial webpack structure to handle html, less, and js

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+'use strict';
+const ExtractText = require('extract-text-webpack-plugin');
+
+let plugins = [
+  new ExtractText('bundle.css'),
+];
+
+module.exports = {
+  entry: `${__dirname}/app/entry.js`,
+  output: {
+    filename: 'bundle.js',
+    path: `${__dirname}/build/`,
+  },
+  plugins: plugins,
+  module: {
+    rules: [
+      //html loader
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+      //less loader
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        use: ExtractText.extract({
+          use: ['css-loader', 'less-loader'],
+        }),
+      },
+      //babel loader
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+      //file loader
+      {
+        test: /\.(woff|svg|eot|ttf).*/,
+        loader: 'url-loader?limit=10000&name=font/[name].[ext]',
+      },
+      {
+        test: /\.(jpg|gif|png)$/,
+        loader: 'file-loader?name=image[hash]-[name].[ext]',
+      },
+    ]
+  },
+};
