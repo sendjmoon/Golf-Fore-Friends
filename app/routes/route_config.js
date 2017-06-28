0eef554d6be8a1a88a796af7f34d074c03b5dbcc
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-06-28 15:27:11 -0700

    change not brought in

--- app/routes/route_config.js
+++ app/routes/route_config.js
@@ -4,7 +4,6 @@ module.exports = function($routeProvider) {
   $routeProvider
     .when('/home', {
       template: require('../html/home.html'),
-      controller: 'HomeController',
     })
     .otherwise({
       redirectTo: '/',
