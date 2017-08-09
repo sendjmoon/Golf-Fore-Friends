446ed4fa79d89cf97c3efc7306b3009fc70b6f14
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-08-09 16:33:32 -0700

    added hamburger menu open and close animation

--- app/angular/controllers/nav_controller.js
+++ app/angular/controllers/nav_controller.js
@@ -3,16 +3,16 @@
 module.exports = function(app) {
   app.controller('NavController', ['$rootScope', 'AuthService', function($rs, AuthService) {
     this.signout = AuthService.signout;
-    let hamMenu = document.getElementById('hamburger-menu-icon');
+    let $hamMenu = $('#hamburger-menu-icon');
+    let $navMenu = $('#nav-menu');
 
-    let toggleMenu = function(el) {
-      let navMenu = document.getElementById('nav-menu');
-      el.classList.toggle('open');
-      navMenu.classList.toggle('open');
+    let toggleMenu = function($el) {
+      $el.toggleClass('open');
+      $navMenu.toggleClass('open');
     };
 
-    hamMenu.addEventListener('click', () => {
-      toggleMenu(hamMenu);
+    $hamMenu.on('click', () => {
+      toggleMenu($hamMenu);
     });
   }]);
 };
