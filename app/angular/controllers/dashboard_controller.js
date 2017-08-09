41f1bf5698ba0ffd8f9e9cab6b119378641568b6
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-08-09 14:05:33 -0700

    add hamburger icon at 991 width. add animation for open close

--- app/angular/controllers/dashboard_controller.js
+++ app/angular/controllers/dashboard_controller.js
@@ -2,6 +2,8 @@
 
 module.exports = function(app) {
   app.controller('DashboardController', [function() {
-
+    $('.hamburger-menu-icon').on('click', function() {
+      $(this).toggleClass('open');
+    });
   }]);
 };
