ce42fa1a042162878aa3631bca7ba3c93fe68fea
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-10-13 23:42:58 -0700

    reordered call stack for functions in dashboard controller

--- app/angular/controllers/dashboard_controller.js
+++ app/angular/controllers/dashboard_controller.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
       total = total / gamesArray.length;
     };
 
-    this.calcHandicap();
     AuthService.checkSessionExists();
+    this.calcHandicap();
   }]);
 };
