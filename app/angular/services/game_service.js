71f3b369a51a5fd1c3f8c92b87ec788acf85fe8c
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-11-06 17:13:22 -0800

    add service to get all of user's friends. then added another service to use that data and rank players based on their handicap

--- app/angular/services/game_service.js
+++ app/angular/services/game_service.js
@@ -28,5 +28,35 @@ module.exports = function(app) {
           });
       });
     };
+
+    this.getAllFriends = function() {
+      return new Promise((resolve, reject) => {
+        //TODO: change list to all
+        $http.get('/friends/list')
+          .then((allFriends) => {
+            this.friendsArray = allFriends.data;
+            resolve(this.friendsArray);
+          })
+          .catch((err) => {
+            alert('error getting friends list');
+            reject();
+          });
+      });
+    };
+
+    this.rankPlayers = function(playersArray) {
+      return new Promise((resolve, reject) => {
+        let rankedArray = [];
+        rankedArray.push($rs.user);
+        for (let i = 0; i < playersArray.length; i++) {
+          if (playersArray[i].handicap < rankedArray[i].handicap) {
+            rankedArray.unshift(playersArray[i]);
+          } else {
+            rankedArray.push(playersArray[i]);
+          }
+        }
+        resolve(rankedArray);
+      });
+    };
   }]);
 };
