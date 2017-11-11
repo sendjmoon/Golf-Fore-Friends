48e490e8f8be6cb838a42ce1629bbcaf60132ce1
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-11-11 02:35:26 -0800

    create plugin for chartjs to draw horizontal line on chart representing user handicap

--- app/angular/controllers/chart_controller.js
+++ app/angular/controllers/chart_controller.js
@@ -52,29 +52,82 @@ module.exports = function(app) {
 
     this.loadChart = function() {
       let Chart = require('chart.js');
-      let ctx = document.getElementById('gameChart');
-      let chartOptions = {
-        type: 'line',
-        data: {
-          labels: dateData,
-          datasets: [{
+      let canvas = document.getElementById('gameChart')
+      let ctx = canvas.getContext('2d');
+      let userHandicap = JSON.parse(window.sessionStorage.getItem('currentUser')).handicap;
+
+      let handicapLinePlugin = {
+        afterDraw: function(chartInstance) {
+          let yScale = chartInstance.scales['y-axis-0'];
+          let canvas = chartInstance.chart;
+          let ctx = canvas.ctx;
+          let index;
+          let line;
+          let style;
+          let yValue;
+
+          if (chartInstance.options.horizontalLine) {
+            for (index = 0; index < chartInstance.options.horizontalLine.length; index++) {
+              line = chartInstance.options.horizontalLine[index];
+
+              !line.style ? style = 'rgba(169, 169, 169, 0.6)' : style = line.style;
+              line.y ? yValue = yScale.getPixelForValue(line.y) : yValue = 0;
+
+              ctx.lineWidth = 4;
+
+              if (yValue) {
+                ctx.beginPath();
+                ctx.moveTo(38, yValue);
+                ctx.lineTo(canvas.width - 36, yValue);
+                ctx.strokeStyle = style;
+                ctx.stroke();
+              }
+
+              if (line.text) {
+                ctx.fillStyle = style;
+                ctx.fillText(line.text, 0, yValue + ctx.lineWidth);
+              }
+            }
+            return;
+          }
+        }
+      };
+
+      Chart.pluginService.register(handicapLinePlugin);
+
+      let chartData = {
+        labels: dateData,
+        datasets: [
+          {
             label: 'Strokes',
+            backgroundColor: 'rgba(77, 187, 51, 0.2)',
+            borderColor: 'rgba(77, 187, 51, 1)',
+            borderWidth: 2,
             data: strokeData,
-            options: {
-              reverse: true,
+          },
+        ],
+      };
+
+      let gameChart = new Chart(ctx, {
+        type: 'line',
+        data: chartData,
+        options: {
+          responsive: true,
+          legend: {
+            position: 'bottom',
+          },
+          elements: {
+            line: {
+              tension: 0.1,
             },
-            backgroundColor: [
-              'rgba(77, 187, 51, 0.2)',
-            ],
-            borderColor: [
-              'rgba(77, 187, 51, 1)',
-            ],
-            borderWidth: 1,
+          },
+          horizontalLine: [{
+            y: userHandicap,
+            style: 'rgba(254, 172, 0, 0.5)',
           }],
         },
-      };
-      let myChart = new Chart(ctx, chartOptions);
-    }
+      });
+    };
 
   }]);
 };
