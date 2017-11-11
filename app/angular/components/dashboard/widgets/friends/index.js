2fc8017f4a1a2aaffd3fd6bcc207c4da2e90b30c
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-11-11 13:10:06 -0800

    remove unused components

--- app/angular/components/dashboard/widgets/friends/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(app) {
-  app.component('friendsWidget', {
-    template: require('./friends-widget-template.html'),
-    // controller: 'DashboardController',
-    // controllerAs: 'dc',
-  });
-};
