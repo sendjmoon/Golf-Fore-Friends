3966a6b7a09f566c4a1b6fd529f006e32d281df6
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-11-22 02:45:28 -0800

    use jquery to handle avatar input event listener. add gridfs-stream npm

--- app/angular/controllers/settings_controller.js
+++ app/angular/controllers/settings_controller.js
@@ -2,6 +2,10 @@
 
 module.exports = function(app) {
   app.controller('SettingsController', ['$rootScope', function($rs) {
-    this.user = $rs.user;
+    this.user = JSON.parse(window.sessionStorage.getItem('currentUser'));
+    
+    $('#avatar-input').change(function() {
+      console.log($(this).prop('files'));
+    })
   }]);
 }
