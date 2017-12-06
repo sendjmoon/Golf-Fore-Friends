3883b33d62a6d89f9a9233a38187c9e0d27377ea
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-12-06 04:12:12 -0800

    Write quick function to handle parallax effects for navbar.

--- app/angular/controllers/nav_controller.js
+++ app/angular/controllers/nav_controller.js
@@ -4,7 +4,11 @@ module.exports = function(app) {
   app.controller('NavController', ['$rootScope', 'AuthService', function($rs, AuthService) {
     this.signout = AuthService.signout;
 
+    let $window = $(window);
     let $hamMenu = $('#hamburger-menu-icon');
+    let $banner = $('.banner-container');
+    let $navBar = $('nav-bar');
+    let $gffNav = $('.gff-nav');
     let $navMenu = $('#nav-menu');
 
     this.toggleMenu = function($el) {
@@ -12,8 +16,33 @@ module.exports = function(app) {
       $navMenu.toggleClass('open');
     };
 
+    this.backgroundParallax = function() {
+      let offsetTop = 0;
+
+      $window.on('scroll', () => {
+        offsetTop = $navBar.offset().top;
+        offsetTop = 15 - ((offsetTop - 15) * 0.2);
+
+        if (offsetTop < 0) offsetTop = 0;
+        if (offsetTop > 15) offsetTop = 15;
+
+        if (offsetTop === 0) {
+          $gffNav.css('width', '100%');
+          $navBar.css('height', '60px');
+        }
+        else {
+          $gffNav.css('width', '97%');
+          $navBar.css('height', '80px');
+        }
+
+        $navBar.css('top', offsetTop + 'px');
+      });
+    }
+
     $hamMenu.on('click', () => {
       this.toggleMenu($hamMenu);
     });
+
+    this.backgroundParallax();
   }]);
 };
