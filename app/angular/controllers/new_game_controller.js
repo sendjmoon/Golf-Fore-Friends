3cb34a917b8e0d61e37e1dbe40e8c4a1e5a4e1eb
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-12-12 15:33:46 -0800

    Add new game controller to handle creating new game.

---
+++ app/angular/controllers/new_game_controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function(app) {
+  app.controller('NewGameController', ['$rootScope', function($rs) {
+
+    let ctrl = this;
+    ctrl.players = [];
+
+    ctrl.init = function() {
+      
+    }
+  }]);
+}
