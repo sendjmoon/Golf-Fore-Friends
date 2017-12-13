b6d2c64cd71518c92c2c9f7ab19a5e5ee1571b7d
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-12-12 16:42:17 -0800

    Remove unused injectors.

--- app/angular/controllers/games_controller.js
+++ app/angular/controllers/games_controller.js
@@ -1,15 +1,14 @@
 'use strict';
 
 module.exports = function(app) {
-  app.controller('GamesController', ['$scope', 'UserService', 'FriendService', 'GameService', 'SearchService', function($scope, UserService, FriendService, GameService, SearchService) {
+  app.controller('GamesController', ['$scope', function($scope) {
 
     let ctrl = this;
-    ctrl.user = UserService.data.user;
     ctrl.creating = false;
 
     ctrl.initCreate = function() {
       ctrl.creating = true;
     }
-    
+
   }]);
 };
