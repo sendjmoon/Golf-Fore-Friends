85be82d1ebf42f5fde7a58375f2c2ef546d2a98c
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-12-20 17:00:15 -0800

    Call single gameService method to create a game.

--- app/angular/controllers/create_game_controller.js
+++ app/angular/controllers/create_game_controller.js
@@ -4,7 +4,6 @@ module.exports = function(app) {
   app.controller('CreateGameController', ['$rootScope', '$route', 'UserService', 'FriendService', 'GameService', 'ResultService', function($rs, $route, userService, friendService, gameService, resultService) {
 
     const ctrl = this;
-    const async = require('async');
     ctrl.user = userService.data.user;
     ctrl.allFriends = friendService.data.allFriends;
     ctrl.players = [];
@@ -12,34 +11,23 @@ module.exports = function(app) {
 
     ctrl.createGame = function(gameData) {
       gameData.players = ctrl.players;
-      gameService.create(gameData)
-        .then((newGame) => {
-          resultService.create(newGame._id, gameData.players)
-            .then((newResults) => {
-              $route.reload();
-            });
-        })
+      gameService.newGame(gameData)
         .catch((err) => {
+          console.log('Error creating game.');
           console.log(err);
         });
-    }
-
-    // let userUpdateData = {
-    //   usersArray: resultsArray,
-    //   updateQuery: { $addToSet: { gameIds: newGame._id }},
-    // };
-    // userService.updateMany(userUpdateData)
+    };
 
     ctrl.addUser = function(user) {
       let friendsArray = ctrl.allFriends.friends;
       ctrl.players.push(user);
       friendsArray.splice(friendsArray.indexOf(user), 1);
-    }
+    };
 
     ctrl.removeUser = function(user) {
       ctrl.players.splice(ctrl.players.indexOf(user), 1);
       ctrl.allFriends.friends.push(user);
-    }
+    };
 
     ctrl.init = function() {
       let userData = {
