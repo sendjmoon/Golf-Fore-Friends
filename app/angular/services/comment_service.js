f5893d668433df7ea4f182166a5ed1999825b035
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-12-29 13:11:26 -0800

    Create comment service.

---
+++ app/angular/services/comment_service.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function(app) {
+  app.factory('CommentService', ['$rootScope', '$http', function($rs, $http) {
+
+    const create = function(gameId, authorId, authorName, content) {
+      return new Promise((resolve, reject) => {
+        const commentData = {
+          gameId: gameId,
+          authorId: authorId,
+          authorName: authorName,
+          content: content,
+        };
+
+        $http.post(`${$rs.baseUrl}/games/comments/create`, commentData)
+          .then(resolve)
+          .catch(reject);
+      });
+    };
+
+    return {
+      create: create,
+    };
+  }]);
+};
