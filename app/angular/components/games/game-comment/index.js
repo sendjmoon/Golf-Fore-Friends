c3a2b897a2d5d20443fb519b39cce74e81bb77a0
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-12-29 13:11:55 -0800

    Create comment component.

---
+++ app/angular/components/games/game-comment/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function(app) {
+  app.component('comment', {
+    template: require('./game-comment-template.html'),
+    controller: 'CommentController',
+    controllerAs: 'cc',
+    bindings: {},
+  });
+};
