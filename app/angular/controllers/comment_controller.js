f4c5b3e87f326d2ce34223f7b6ec19f7dc81fb4a
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-12-29 18:58:41 -0800

    After creating a new comment, the new comment refId is added to the game and user arrays of comment Ids.

--- app/angular/controllers/comment_controller.js
+++ app/angular/controllers/comment_controller.js
@@ -1,23 +1,28 @@
 'use strict';
 
 module.exports = function(app) {
-  app.controller('CommentController', ['$rootScope', '$scope', 'CommentService', function($rs, $scope, commentService) {
+  app.controller('CommentController', ['$rootScope', '$scope', 'CommentService', 'GameService', 'UserService', function($rs, $scope, commentService, gameService, userService) {
 
     let ctrl = this;
+    ctrl.user = userService.data.user;
 
-    ctrl.create = function(commentData) {
-      commentService.create(
-        commentData.gameId,
-        commentData.authorId,
-        commentData.authorName,
-        commentData.content
-      )
-        .then((res) => {
-          console.log('woohoo');
-          console.log(res);
+    ctrl.create = function(gameId, content) {
+      commentService.create(gameId, ctrl.user._id, ctrl.user.fullName, content)
+        .then((newComment) => {
+          let updateOptions = {
+            $addToSet: { comments: newComment._id },
+          };
+          gameService.updateById(gameId, updateOptions)
+            .catch(() => {
+              console.log('Error updating game.');
+            });
+          userService.updateByEmailOrUsername(ctrl.user.email, updateOptions)
+            .catch(() => {
+              console.log('Error updating user.');
+            });
         })
         .catch((err) => {
-          console.log('damn');
+          console.log('Error posting comment.');
         });
     };
   }]);
