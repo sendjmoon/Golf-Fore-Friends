9114cb111ca1325688750ca8b55bf92b9918c8b1
JAMES MOON <SENDJMOON@GMAIL.COM>
2018-07-26 16:16:25 -0700

    Fix syntax error.

--- app/app.js
+++ app/app.js
@@ -41,7 +41,7 @@ if (process.env.REDISTOGO_URL) {
   var rtg = require('url').parse(process.env.REDISTOGO_URL);
   var redisClient = redis.createClient(rtg.port, rtg.hostname);
 
-  redis.auth(rtg.auth.split(':')[1]);
+  redisClient.auth(rtg.auth.split(':')[1]);
 } else {
   var redisClient = redis.createClient();
 }
