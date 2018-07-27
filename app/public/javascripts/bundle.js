d223fbf3840008346580a44a9e453d1673c0c983
JAMES MOON <SENDJMOON@GMAIL.COM>
2018-07-27 00:39:34 -0700

    Change API url.

--- app/public/javascripts/bundle.js
+++ app/public/javascripts/bundle.js
@@ -17536,8 +17536,8 @@ __webpack_require__(132);
 
 var golfApp = angular.module('golfApp', ['ngRoute', 'ngRoute.middleware']);
 
-// let API_URL = 'https://golf-fore-friends.herokuapp.com';
-var API_URL = 'http://localhost:3000';
+var API_URL = 'https://golf-fore-friends.herokuapp.com';
+// const API_URL = 'http://localhost:3000';
 
 __webpack_require__(133)(golfApp);
 __webpack_require__(143)(golfApp);
