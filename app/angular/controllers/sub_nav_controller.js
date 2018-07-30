d350be01b366cc86557e9150b3c03b883e9cc198
JAMES MOON <SENDJMOON@GMAIL.COM>
2018-07-30 13:37:24 -0700

    Dashboard scrolls to anchor link when clicked.

--- app/angular/controllers/sub_nav_controller.js
+++ app/angular/controllers/sub_nav_controller.js
@@ -2,6 +2,23 @@
 
 module.exports = function(app) {
   app.controller('SubNavController', ['$rootScope', function($rs) {
+    const ctrl = this;
 
+    ctrl.scrollToAnchor = function(anchorId, href) {
+      anchorId = `#${anchorId}`;
+      $('html, body').animate({ scrollTop: $(anchorId).offset().top }, 500);
+    }
+
+    ctrl.subNavClickHandler = function() {
+      $('.sub-nav-link').on('click', function(e) {
+        ctrl.scrollToAnchor($(this).attr('scroll-target'), $(this).attr('href'));
+      });
+    }
+
+    ctrl.init = function() {
+      ctrl.subNavClickHandler();
+    }
+
+    ctrl.init();
   }]);
 };
